d9374dce818eaebfcc34206d032f642da5f9f0a8
hard34 <derbin398@gmail.com>
2022-04-12 19:00:22 +0100

    n-ое число Фибоначчи при условии, что последовательность начинается с (1, 1, ...). Например, ib(10) = 55. Программа должна вывести вычисленное значение в стандартный вывод;

---
+++ fibanach.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n){
+    int c, a = 1;
+    int b = 1;
+    for(int i = 3; i <= n; i++){
+        c = b;
+        b = a + b;
+        a = c;
+    }
+    return b;
+}
+
+int main(){
+    cout << fib(10) << endl;
+    return 0;
+}
